bfd78ac0de2f2de4fb123a3c29af6815d0f63120
David Hormiga Afonso <davidhormigafonso@gmail.com>
2022-10-16 00:52:21 +0100

    deploy

--- src/components/BlogPosts/index.jsx
+++ src/components/BlogPosts/index.jsx
@@ -1,6 +1,6 @@
 import { postVariablesJS } from './VariablesJS/VariablesJS'
 import { postHelloWorld } from './HelloWorld/HelloWorld'
 
-const BlogPosts = [postVariablesJS, postHelloWorld]
+const BlogPosts = [postVariablesJS, postHelloWorld, postVariablesJS, postHelloWorld]
 
 export default BlogPosts
