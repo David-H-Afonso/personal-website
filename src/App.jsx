af28a06668a9aed0e1ec6f02dad3cfd6417fc7e8
David Hormiga Afonso <davidhormigafonso@gmail.com>
2022-10-15 18:23:24 +0100

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloading, fuck yeah! Vivan
-          los webhooks
-        </p>
+        <p>A llorar a la calle de la llorería</p>
         <a
           className="App-link"
           href="https://reactjs.org"
