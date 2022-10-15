730747c2001badd2c70d4a3b15529ba94056061a
David Hormiga Afonso <davidhormigafonso@gmail.com>
2022-10-15 16:39:10 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Or dont
+          Edit <code>src/App.js</code> and save to reload. Or dont New Build
         </p>
         <a
           className="App-link"
--- windows-build.sh
+++ windows-build.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 git checkout develop
 git pull origin develop
+git add .
+git commit -m "deploy"
 yarn build
 git push origin develop
 git checkout master
 git fetch --all
-git checkout origin/develop -- src/build/
+git checkout origin/develop -- build/
+git mv (gci build/*) .
+git rm build/
+git add .
+git commit -m "deploy"
 git push origin master
 git checkout develop
