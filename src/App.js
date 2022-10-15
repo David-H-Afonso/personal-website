2536374e57fcbd31c1148114f0209278b05963b4
David Hormiga Afonso <davidhormigafonso@gmail.com>
2022-10-15 17:33:43 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloading
+          Edit <code>src/App.js</code> and save to reloading, fuck yeah!
         </p>
         <a
           className="App-link"
