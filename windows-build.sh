8f108d9628b5ced15177ac45b3d763924fff2b4a
David Hormiga Afonso <davidhormigafonso@gmail.com>
2022-10-15 17:11:52 +0100

    deploy

--- windows-build.sh
+++ windows-build.sh
@@ -7,6 +7,8 @@ git add .
 git commit -m "deploy"
 git push origin develop
 git checkout master
+git pull origin master
+git rm -r *
 git fetch --all
 git checkout origin/develop -- build/
 git mv (gci build/*) .
