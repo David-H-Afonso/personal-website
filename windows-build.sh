a5c47cb0e9ffb5a1aef171af2223d028c18500f6
David Hormiga Afonso <davidhormigafonso@gmail.com>
2022-10-15 16:46:46 +0100

    deploy

--- windows-build.sh
+++ windows-build.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 git checkout develop
 git pull origin develop
-git add .
-git commit -m "deploy"
 yarn
 yarn build
+git add .
+git commit -m "deploy"
 git push origin develop
 git checkout master
 git fetch --all
 git checkout origin/develop -- build/
 git mv (gci build/*) .
-git rm build/
 git add .
 git commit -m "deploy"
 git push origin master
