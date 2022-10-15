b956c4edd41929ac0e4580de6e48576a18945c04
David Hormiga Afonso <davidhormigafonso@gmail.com>
2022-10-15 18:11:03 +0100

    deploy

--- windows-build.ps1
+++ windows-build.ps1
@@ -7,3 +7,14 @@ git commit -m "deploy"
 git push origin develop
 cd ".."
 cd "D:\Programacion\deploy-personal-website"
+git pull origin master
+git rm -r *
+git fetch --all
+git checkout origin/develop -- build/
+git mv (gci build/*) .
+git add .
+git commit -m "deploy"
+git push origin master
+cd ".."
+cd "D:\Programacion\personal-website"
+git checkout develop
