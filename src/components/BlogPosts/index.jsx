6c6b58b3ead9ccd4335f26e8159c4b7dce0bd109
David Hormiga Afonso <davidhormigafonso@gmail.com>
2022-10-21 20:17:23 +0100

    delete

--- src/components/BlogPosts/index.jsx
+++
@@ -1,6 +0,0 @@
-import { postVariablesJS } from './VariablesJS/VariablesJS'
-import { postHelloWorld } from './HelloWorld/HelloWorld'
-
-const BlogPosts = [postVariablesJS, postHelloWorld, postVariablesJS, postHelloWorld]
-
-export default BlogPosts
