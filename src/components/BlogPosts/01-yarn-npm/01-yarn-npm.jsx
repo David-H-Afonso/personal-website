9b88e7e64973d3c246c44402ddc6b90b59acfea6
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-01-01 20:50:00 +0000

    deploy

--- src/components/BlogPosts/01-yarn-npm/01-yarn-npm.jsx
+++ src/components/BlogPosts/01-yarn-npm/01-yarn-npm.jsx
@@ -8,27 +8,41 @@ console.log('Hola mundo!')
 const NpmYarn = (props) => {
 	return (
 		<BaseBlog postInfo={postNpmYarn}>
-			<h2>Esto 222222es un ejemplo de cuerpo de un blog!!</h2>
 			<p>
-				¿Quieres crear tu primer comando por consola y conseguir tu tan esperado <b>hola mundo</b>?!
-			</p>
-			<p>Haz lo siguiente 👇🏻</p>
-			<Code>
-				<Comment>That's how you make your first hello world! And show it in the console</Comment>
+				If you're a <strong>JavaScript developer</strong>, you've probably heard of npm and yarn.
+				These are both popular package managers that help you manage the dependencies of your
+				projects, but they have some key differences that you should be aware of.
 				<br />
-				<VariableName>console</VariableName>.<SymbolsFuncitons>log(</SymbolsFuncitons>
-				<StringCode>"Hello World!"</StringCode>
-				<SymbolsFuncitons>)</SymbolsFuncitons>
-			</Code>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores labore eum esse id harum
-				quo officiis nesciunt temporibus iste, deleniti libero tempora magnam? Necessitatibus
-				voluptates, blanditiis veritatis vero quae eum? Lorem ipsum dolor sit amet consectetur,
-				adipisicing elit. Cumque, eaque corporis saepe amet voluptatum quos dicta vel nostrum culpa,
-				sit dolores impedit a, veritatis minima numquam et accusantium? Architecto, tempore?Lorem
-				ipsum, dolor sit amet consectetur adipisicing elit. Dolores repudiandae iste quae atque,
-				sapiente corporis. Fugiat, pariatur alias cupiditate hic repudiandae officiis, nihil
-				excepturi quas, quia sit fugit vitae tempore?
+				<br />
+				First off, let's talk about performance. When it comes to installing packages, yarn has a
+				bit of an edge over npm. That's because yarn installs packages sequentially, rather than
+				concurrently like npm does. This means that yarn can take advantage of cache and avoid
+				making unnecessary network requests for packages that have already been installed. As a
+				result, yarn may be faster at installing packages, especially for projects with a large
+				number of dependencies.
+				<br />
+				<br />
+				But speed isn't the only thing that sets yarn apart from npm. Yarn also has a deterministic
+				dependency resolution algorithm, which means that the order in which packages are installed
+				is always the same. This can be a big advantage in terms of reliability and reproducibility,
+				since you can be confident that your project will behave the same way no matter where or how
+				you install it.
+				<br />
+				<br />
+				Security is another area where yarn shines. It has features like workspaces and automatic
+				detection of malicious packages that npm doesn't have. This can give you some extra peace of
+				mind when working on projects with sensitive dependencies.
+				<br />
+				<br />
+				Of course, npm is still a great package manager, and it has a lot going for it. It's widely
+				used, so it's often easier to find help or resources related to npm. And with a larger
+				ecosystem of packages, npm may be more compatible with certain libraries or tools.
+				<br />
+				<br />
+				So which one should you use? Ultimately, the choice between npm and yarn comes down to your
+				personal preference and the needs of your project. Both are powerful tools, and either one
+				can help you manage your dependencies effectively. So, it's up to you to decide which one
+				suits you the best.
 			</p>
 		</BaseBlog>
 	)
