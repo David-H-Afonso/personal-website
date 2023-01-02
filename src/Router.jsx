59b8931c4b55bf4c1f501db1ef66ce8d40c54b48
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-01-02 09:18:26 +0000

    Hide and remove the placeholder blogposts

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,8 +3,8 @@ import { createBrowserRouter } from 'react-router-dom'
 import Home from './components/Home/container/Home'
 import Blog from './components/Blog/container/Blog'
 
-import VariablesJS, { postVariablesJS } from './components/BlogPosts/VariablesJS/VariablesJS'
-import HelloWorld, { postHelloWorld } from './components/BlogPosts/HelloWorld/HelloWorld'
+// import VariablesJS, { postVariablesJS } from './components/BlogPosts/VariablesJS/VariablesJS'
+// import HelloWorld, { postHelloWorld } from './components/BlogPosts/HelloWorld/HelloWorld'
 import NpmYarn, { postNpmYarn } from './components/BlogPosts/01-yarn-npm/01-yarn-npm'
 
 export default createBrowserRouter([
@@ -16,14 +16,14 @@ export default createBrowserRouter([
 		path: '/blog',
 		element: <Blog />,
 	},
-	{
-		path: postVariablesJS.url,
-		element: <VariablesJS />,
-	},
-	{
-		path: postHelloWorld.url,
-		element: <HelloWorld />,
-	},
+	// {
+	// 	path: postVariablesJS.url,
+	// 	element: <VariablesJS />,
+	// },
+	// {
+	// 	path: postHelloWorld.url,
+	// 	element: <HelloWorld />,
+	// },
 	{
 		path: postNpmYarn.url,
 		element: <NpmYarn />,
--- src/components/BlogPosts/index.jsx
+++ src/components/BlogPosts/index.jsx
@@ -1,7 +1,5 @@
-import { postVariablesJS } from './VariablesJS/VariablesJS'
-import { postHelloWorld } from './HelloWorld/HelloWorld'
 import { postNpmYarn } from './01-yarn-npm/01-yarn-npm'
 
-const BlogPosts = [postNpmYarn, postVariablesJS, postHelloWorld]
+const BlogPosts = [postNpmYarn]
 
 export default BlogPosts
