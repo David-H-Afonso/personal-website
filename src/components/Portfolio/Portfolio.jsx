3b21519699e490ee3ed8dbcb0a704b6555f0a33b
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-04 21:36:32 +0000

    Create Portfolio & Projects base files

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Portfolio.css'
+import Projects from '../Projects/Projects'
+
+const Portfolio = () => {
+	return (
+		<div className='portfolioWrapper'>
+			<h1>Portfolio</h1>
+			<Projects />
+		</div>
+	)
+}
+
+export default Portfolio
