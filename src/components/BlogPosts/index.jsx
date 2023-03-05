94f571e896316189fc37ae039400e17803c61224
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-05 01:29:50 +0000

    Revert "deploy"
    This reverts commit 49e2e09967a48f1f86febe4b4d991f4b4cf0941a.

---
+++ src/components/BlogPosts/index.jsx
@@ -0,0 +1,7 @@
+import { postNpmYarn } from './01-yarn-npm/01-yarn-npm'
+import { postArrayJS } from './02-array-js/02-array-js'
+import { postAsyncAwait } from './03-async-await/03-async-await'
+
+const BlogPosts = [postNpmYarn, postArrayJS, postAsyncAwait]
+
+export default BlogPosts
---
+++ src/components/elements/Post/logos/index.jsx
@@ -0,0 +1,6 @@
+export const logos = {
+	hardware: require('./hardware_logo.png'),
+	js: require('./js_logo.png'),
+	react: require('./react_logo.png'),
+	node: require('./node_logo.png'),
+}
