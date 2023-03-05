7db5e2057c9a72c5fbb6cedb5c0e2576c5c77193
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-05 01:34:01 +0000

    deploy

--- src/components/BlogPosts/index.jsx
+++
@@ -1,7 +0,0 @@
-import { postNpmYarn } from './01-yarn-npm/01-yarn-npm'
-import { postArrayJS } from './02-array-js/02-array-js'
-import { postAsyncAwait } from './03-async-await/03-async-await'
-
-const BlogPosts = [postNpmYarn, postArrayJS, postAsyncAwait]
-
-export default BlogPosts
