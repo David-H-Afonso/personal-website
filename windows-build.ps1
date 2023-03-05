baf762955815dfdcdc99a6305cec427f48e126b3
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-05 01:27:10 +0000

    Test new deploy command is working

---
+++ windows-build.ps1
@@ -0,0 +1,21 @@
+git checkout master
+git pull origin master
+yarn
+yarn build
+git add .
+git commit -m "deploy"
+git push origin master
+Set-Location ".."
+Set-Location "D:\Programacion\personal-website\deploy-personal-website"
+git pull origin master
+git stash
+git rm -r *
+git fetch --all
+git checkout origin/develop -- build/
+git mv (Get-ChildItem build/*) .
+git add .
+git commit -m "deploy"
+git push origin master
+Set-Location ".."
+Set-Location "D:\Programacion\personal-website\personal-website"
+git checkout develop
