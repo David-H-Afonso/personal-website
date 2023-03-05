289a850a12036e2b2122dce8dcf3c311e8191d28
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-05 10:36:57 +0000

    Fix console error: <a> nested

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -4,8 +4,12 @@ import './Project.css'
 const Project = (props) => {
 	const { project } = props
 
+	const navigateToDeploy = () => {
+		window.open(project.deploy, '_blank')
+	}
+
 	return (
-		<a className='project' target='_blank' rel='noreferrer' href={project.deploy}>
+		<div className='project' onClick={navigateToDeploy}>
 			<img src={project.image} alt={project.title} className='projectImage' />
 			<h1 className='projectTitle'>{project.title}</h1>
 			<p className='projectDescription'>{project.description}</p>
@@ -13,7 +17,7 @@ const Project = (props) => {
 				<p>See Github Repository</p>
 				<img src={require('../../assets/github.png')} alt='github' style={{ width: 20, marginLeft: 5 }} />
 			</a>
-		</a>
+		</div>
 	)
 }
 
