e65509b235312e7bb1ddd6f4199439e47219403c
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-05 10:37:12 +0000

    Sort by highlight added

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,15 +4,21 @@ import Projects from '../Projects/Projects'
 import Project from '../Projects/Project'
 
 const Portfolio = () => {
+	const highlightProjects = []
 	const sortedProjects = Projects.sort((a, b) => {
 		return a.id - b.id
+	}).filter((project) => {
+		if (project.highlight) {
+			highlightProjects.push(project)
+		}
+		return !project.highlight
 	})
 
 	return (
 		<div className='portfolioWrapper'>
 			<h1 style={{ textAlign: 'center' }}>Check out some of my creations</h1>
 			<div className='projectsContainer'>
-				{sortedProjects.reverse().map((project) => (
+				{[...sortedProjects, ...highlightProjects].reverse().map((project) => (
 					<Project key={project.id} project={project} />
 				))}
 			</div>
