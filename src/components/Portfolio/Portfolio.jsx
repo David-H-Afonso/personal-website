56815fbcd360b0df54f81f52bd26090f314d5e80
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-05 01:12:31 +0000

    Display projects in Portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './Portfolio.css'
 import Projects from '../Projects/Projects'
+import Project from '../Projects/Project'
 
 const Portfolio = () => {
+	const sortedProjects = Projects.sort((a, b) => {
+		return a.id - b.id
+	})
+
 	return (
 		<div className='portfolioWrapper'>
 			<h1>Portfolio</h1>
-			<Projects />
+			<div className='projectsContainer'>
+				{sortedProjects.reverse().map((project) => (
+					<Project key={project.id} project={project} />
+				))}
+			</div>
 		</div>
 	)
 }
